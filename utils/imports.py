85147db8602a12635d4d4ee07ed4f89379ceed48
dh <david@cslcap.com>
2025-06-06 23:57:12 -0600

    Create imports.py

---
+++ utils/imports.py
@@ -0,0 +1,22 @@
+# utils/imports.py
+# Standard libraries
+import streamlit as st
+import pandas as pd
+import altair as alt
+from datetime import datetime
+import io
+import hashlib
+
+# Third-party libraries
+from numpy import busday_count
+from xhtml2pdf import pisa
+
+# Project imports
+from utils.config import (
+    get_supabase_client, 
+    PRIMARY_COLOR, 
+    PERFORMANCE_GRADIENT, 
+    RISK_GRADIENT, 
+    COLOR_PALETTE,
+    PLATFORM_FEE_RATE
+)
