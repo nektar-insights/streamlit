aecabd59b10b4a91618dd1b8dde3c4c2dba71eba
dh <david@cslcap.com>
2025-07-29 11:26:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # streamlit_app.py
 from utils.imports import *
-from utils.config import inject_global_styles, inject_logo
+from utils.config import inject_global_styles, inject_logo, get_supabase_client
 
 # ----------------------------
 # Apply Branding
